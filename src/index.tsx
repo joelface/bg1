c2ab702f1b48d0e712d6c1db7fa6a634be797f21
Joel Bruick <joel+git@joelface.com>
2021-04-09 13:53:11 -0400

    Set background/text colors on body

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ document.head.innerHTML += `
   <meta name=viewport content="width=device-width, initial-scale=1, maximum-scale=1">
   <title>BG1</title>
 `;
+document.body.className = 'bg-gray-50 text-black';
 const accessToken = new StoredToken('accessToken');
 const client = new ApiClient(fetchJson, () => accessToken.get());
 render(<App accessToken={accessToken} client={client} />, document.body);
