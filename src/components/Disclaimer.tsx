99b90fab3164a4a43a6ebcb7d957b0e90ea2d07a
Joel Bruick <joel+git@joelface.com>
2021-04-09 14:26:41 -0400

    Edit disclaimer

--- src/components/Disclaimer.tsx
+++ src/components/Disclaimer.tsx
@@ -10,13 +10,14 @@ export default function Disclaimer({
 }): h.JSX.Element {
   return (
     <>
-      <h1 className="text-xl font-semibold">Disclaimer</h1>
+      <h1 className="text-xl font-semibold">Warning!</h1>
       <p>
-        Use at your own risk. BG1 is highly experimental and provided &quot;as
-        is&quot; without warranty of any kind. It could stop working at any time
-        for any reason. There is no guarantee that using BG1 will actually help
-        you obtain a boarding group, and it should not be relied on for this or
-        any other purpose. Always use the official Disney app.
+        Use at your own risk. BG1 is highly experimental, for educational
+        purposes only, and provided &quot;as is&quot; without warranty of any
+        kind. It could stop working at any time for any reason. There is no
+        guarantee that using BG1 will actually help you obtain a boarding group,
+        and it should not be relied on for this or any other purpose. Always use
+        the official Disney app.
       </p>
       <FloatingButton onClick={onAccept}>Accept</FloatingButton>
     </>
