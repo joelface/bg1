a779fa9789bacdba0da870929a2737d23e3fbae6
Joel Bruick <joel+git@joelface.com>
2021-04-28 14:13:16 -0400

    Disable double-tap zooming

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,10 @@ document.head.innerHTML += `
   <meta name=viewport content="width=device-width, initial-scale=1, maximum-scale=1">
   <title>BG1</title>
 `;
-document.body.className = 'bg-gray-50 text-black';
+const body = document.body;
+body.className = 'bg-gray-50 text-black';
+// Used with "touch-action: manipulation" to disable double-tap zoom
+body.addEventListener('click', () => null);
 const accessToken = new StoredToken('accessToken');
 const client = new ApiClient(fetchJson, () => accessToken.get());
-render(<App accessToken={accessToken} client={client} />, document.body);
+render(<App accessToken={accessToken} client={client} />, body);
