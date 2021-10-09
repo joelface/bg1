12ca366b1cbd5cd34278cff3b123f1a40a3f3cc8
Joel Bruick <joel+git@joelface.com>
2021-10-09 00:56:39 -0400

    Use wrapper div for HowToEnter
    Fixes occasional DOM node order glitchiness when switching queues.

--- src/components/HowToEnter.tsx
+++ src/components/HowToEnter.tsx
@@ -1,13 +1,13 @@
-import { h, Fragment } from 'preact';
+import { h } from 'preact';
 
 import { Queue } from '../virtual-queue';
 
 export default function HowToEnter({ queue }: { queue: Queue }): h.JSX.Element {
   return (
-    <>
+    <div>
       {queue.howToEnterMessage.split('\n\n').map((graf, i) => (
         <p key={i}>{graf}</p>
       ))}
-    </>
+    </div>
   );
 }
