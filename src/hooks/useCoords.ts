e3239a334a8dc5fb7367afdf8837c96afd958c21
Joel Bruick <joel+git@joelface.com>
2024-04-11 18:53:14 -0400

    Remove unnecessary IIFE

--- src/hooks/useCoords.ts
+++ src/hooks/useCoords.ts
@@ -9,19 +9,17 @@ export type Coords = readonly [number, number];
 export default function useCoords(): [typeof coords, typeof updateCoords] {
   const [coords, setCoords] = useState<Coords>();
 
-  const updateCoords = useCallback(() => {
-    (async () => {
-      try {
-        const { coords } = await geolocate(DEFAULT_OPTIONS);
-        setCoords(prev =>
-          prev && coords.latitude === prev[0] && coords.longitude === prev[1]
-            ? prev
-            : [coords.latitude, coords.longitude]
-        );
-      } catch {
-        setCoords(undefined);
-      }
-    })();
+  const updateCoords = useCallback(async () => {
+    try {
+      const { coords } = await geolocate(DEFAULT_OPTIONS);
+      setCoords(prev =>
+        prev && coords.latitude === prev[0] && coords.longitude === prev[1]
+          ? prev
+          : [coords.latitude, coords.longitude]
+      );
+    } catch {
+      setCoords(undefined);
+    }
   }, []);
 
   return [coords, updateCoords];
